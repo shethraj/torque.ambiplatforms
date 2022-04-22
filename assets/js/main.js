0e78fb738b094c1c3b448f796dacf54ec3509849
mehta anjani <anjanimehta98@gmail.com>
2022-04-22 12:54:17 +0530

    1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 /**
-* Template Name: Arsha - v4.7.1
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+team torque
+
 */
 (function() {
   "use strict";
